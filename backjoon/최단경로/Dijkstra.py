0147a9374643d2eb3147d2518796c86bc1b7a677
CastleRain <jjangsw12@gmail.com>
2021-11-20 18:09:03 +0900

    1120

--- backjoon/최단경로/Dijkstra.py
+++ backjoon/최단경로/Dijkstra.py
@@ -54,7 +54,16 @@ for _ in range(m):
 
     graph[a].append((b, c))
 
-print(graph)
+def get_smallest_node():
+    min_value = INF
+    index = 0
+    for i in range(1, n+1):
+        #현재 방문을 안했으며 길이가 무한이지 않은 값
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
 def dijkstra(start):
 
     # 시작 노드에 대해서 초기화
@@ -68,6 +77,7 @@ def dijkstra(start):
     for i in range(n-1):
         # 현재 최단 거링가 가장 짧은 노드를 꺼내서, 방문 처리
         now = get_smallest_node()
+
         visited[now] = True
 
         # 현재 노드와 연결된 다른 노드를 확인
@@ -78,15 +88,7 @@ def dijkstra(start):
             if cost < distance[j[0]]:
                 distance[j[0]] = cost
 
-def get_smallest_node():
-    min_value = INF
-    index = 0
-    for i in range(1, n+1):
-        #현재 방문을 안했으며 길이가 무한이지 않은 값
-        if distance[i] < min_value and not visited[i]:
-            min_value = distance[i]
-            index = i
-        return index
+
 
 dijkstra(start)
 
