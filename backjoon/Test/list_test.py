74786d0d5339aaa4d017d9221834a90c9d56bf6a
CastleRain <jjangsw12@gmail.com>
2021-12-19 15:46:29 +0900

    1419

--- backjoon/Test/list_test.py
+++ backjoon/Test/list_test.py
@@ -1,37 +1,6 @@
-data = [13, 15, 14,  17, 18, 16, 16]
-d = 2
-k = 1
+import collections
 
-answer = []
+root = [1,2,3,4,5]
+q = collections.deque([root])
 
-del_count = 0
-pre_data = 0
-
-for i, da in enumerate(data):
-
-    if i == 0 :
-        answer.append(da)
-        continue
-
-    if pre_data == 0:
-        pre_data = da
-    else:
-        if del_count > k:
-            answer.append(pre_data)
-            pre_data = da
-            del_count = 0
-
-        else:
-            if abs(pre_data - da) <= d:
-                del_count += 1
-                pre_data = da
-            else:
-                del_count = 0
-                answer.append(pre_data)
-                pre_data = da
-
-    if len(data)- 1 == i:
-        answer.append(da)
-print(answer)
-
-
+print(q)
